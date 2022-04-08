6ece5a80d7e5bc431b1c832c7afcd381e6ba6695
Eric Chung <eric87kkk@yahoo.com.tw>
2022-04-08 20:06:50 +0800

    Adding .gitignore file

---
+++ Project 2 AirNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 2 AirNote
+//
+//  Created by Eric chung on 2022/4/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
