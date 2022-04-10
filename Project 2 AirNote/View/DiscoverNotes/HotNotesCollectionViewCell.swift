a5bad1def00f7b15cdeae03776231ec5dfa554e8
Eric Chung <eric87kkk@yahoo.com.tw>
2022-04-10 16:37:17 +0800

    Create discover notes  collectionView

---
+++ Project 2 AirNote/View/DiscoverNotes/HotNotesCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  DiscoverNotesCollectionViewCell.swift
+//  Project 2 AirNote
+//
+//  Created by Eric chung on 2022/4/10.
+//
+
+import UIKit
+
+class HotNotesCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
