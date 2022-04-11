52a9541abd0a7bc06d95ef1f64a7ca4a226c1bca
Eric Chung <eric87kkk@yahoo.com.tw>
2022-04-11 21:43:56 +0800

    Adding category collectionview & notes collectionview

--- Project 2 AirNote/View/DiscoverNotes/NotesCollectionViewCell.swift
+++ Project 2 AirNote/View/DiscoverNotes/NotesCollectionViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class HotNotesCollectionViewCell: UICollectionViewCell {
+class NotesCollectionViewCell: UICollectionViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
