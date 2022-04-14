9f87479d70387f376ab1fc2ebbbbdc7367963f7e
Eric Chung <eric87kkk@yahoo.com.tw>
2022-04-14 23:57:15 +0800

    Complete upload notes (except drawing note)

--- Project 2 AirNote/Model/NoteManager.swift
+++ Project 2 AirNote/Model/NoteManager.swift
@@ -8,6 +8,8 @@
 import Foundation
 import Firebase
 import FirebaseFirestoreSwift
+import FirebaseStorage
+import UIKit
 
 class NoteManager {
     
@@ -19,31 +21,77 @@ class NoteManager {
         
         db.collection("Notes").order(by: "createdTime", descending: true).getDocuments() { (querySnapshot, error) in
             
-                if let error = error {
+            if let error = error {
+                
+                completion(.failure(error))
+            } else {
+                
+                var notes = [Note]()
+                
+                for document in querySnapshot!.documents {
                     
-                    completion(.failure(error))
-                } else {
-                    
-                    var notes = [Note]()
-                    
-                    for document in querySnapshot!.documents {
-
-                        do {
-                            if let note = try document.data(as: Note.self, decoder: Firestore.Decoder()) {
-                                
-                                    notes.append(note)
-                                
-                            }
+                    do {
+                        if let note = try document.data(as: Note.self, decoder: Firestore.Decoder()) {
                             
-                        } catch {
+                            notes.append(note)
                             
-                            completion(.failure(error))
                         }
+                        
+                    } catch {
+                        
+                        completion(.failure(error))
                     }
-                    
-                    completion(.success(notes))
                 }
+                
+                completion(.success(notes))
+            }
         }
     }
     
+    func createNote(note: Note, completion: @escaping (Result<String, Error>) -> Void) {
+        
+        let document = db.collection("Notes").document()
+        
+        var note = note
+        
+        do {
+            note.authorId = "eric"
+            note.comments = []
+            let date = Date()
+            let calendar = Calendar.current
+            let hour = calendar.component(.hour, from: date)
+            let minutes = calendar.component(.minute, from: date)
+            note.createdTime = date
+            note.noteId = document.documentID
+            note.likes = []
+            note.noteClicks = []
+            
+            try  document.setData(from: note, encoder: Firestore.Encoder())
+        }
+        catch {
+            completion(.failure(error))
+        }
+        
+    }
+    
+    func uploadPhoto(image: UIImage, completion: @escaping (Result<URL, Error>) -> Void) {
+        
+        let riversRef = Storage.storage().reference().child(UUID().uuidString + ".jpg")
+        
+        guard let data = image.jpegData(compressionQuality: 0.9) else { return }
+        
+        let uploadTask = riversRef.putData(data, metadata: nil) { (metadata, error) in
+            guard let metadata = metadata else {
+               print("upload failed")
+                return
+            }
+            riversRef.downloadURL { (url, error) in
+                guard let downloadURL = url else {
+                   print("download url failed")
+                    return
+                }
+                completion(.success(downloadURL))
+            }
+        }
+    }
 }
